57a0c19d4723ad4a07d14f6c5391c6abdd65ff86
monkeymc <mc.daikazoku@gmail.com>
2022-11-01 18:07:40 +0700

    list cmd

--- app/src/main/java/com/example/sphere/train/MainActivity.kt
+++ app/src/main/java/com/example/sphere/train/MainActivity.kt
@@ -1,12 +1,15 @@
 package com.example.sphere.train
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.LinearLayoutCompat
 import th.or.gistda.sphere.SphereMap
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var sphere: SphereMap
+    private lateinit var list: LinearLayoutCompat
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -15,6 +18,7 @@ class MainActivity : AppCompatActivity() {
 
     private fun setupView() {
         setupSphere()
+        setupList()
     }
 
     private fun setupSphere() {
@@ -24,5 +28,9 @@ class MainActivity : AppCompatActivity() {
 
     private fun initSphere() {
         sphere.load("test2022 ", packageName)
+    }
+
+    private fun setupList() {
+        list = findViewById(R.id.list)
     }
 }
