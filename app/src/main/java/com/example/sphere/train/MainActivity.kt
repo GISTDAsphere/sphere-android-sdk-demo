2b0afcc8c35bb247a109962a4024b100d4c2bc42
monkeymc <mc.daikazoku@gmail.com>
2022-11-01 15:24:40 +0700

    init sphere

--- app/src/main/java/com/example/sphere/train/MainActivity.kt
+++ app/src/main/java/com/example/sphere/train/MainActivity.kt
@@ -2,10 +2,20 @@ package com.example.sphere.train
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import th.or.gistda.sphere.SphereMap
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setupView()
+    }
+
+    private fun setupView() {
+        setupSphere()
+    }
+
+    private fun setupSphere() {
+        // TODO findViewById<SphereMap>(R.id.sphere).load("", "")
     }
 }
