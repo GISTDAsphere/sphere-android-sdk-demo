3d0a42f68a8c0c45a1e819dadf86f2c4de670756
monkeymc <mc.daikazoku@gmail.com>
2022-11-08 01:04:27 +0700

    event

--- app/src/main/java/com/example/sphere/train/MainActivity.kt
+++ app/src/main/java/com/example/sphere/train/MainActivity.kt
@@ -1,8 +1,11 @@
 package com.example.sphere.train
 
 import android.os.Bundle
+import android.util.Log
+import android.webkit.JavascriptInterface
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.LinearLayoutCompat
+import com.google.gson.JsonParser
 import th.or.gistda.sphere.SphereMap
 
 class MainActivity : AppCompatActivity() {
@@ -27,7 +30,7 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun initSphere() {
-        sphere.load("test2022", packageName)
+        sphere.load("test2022", packageName, this)
     }
 
     private fun setupList() {
@@ -40,4 +43,39 @@ class MainActivity : AppCompatActivity() {
             list.addView(it)
         }
     }
+
+    @JavascriptInterface
+    fun ready(result: String) {
+        sphere.call("Layers.add", SphereMap.SphereStatic("Layers", "TRAFFIC"))
+    }
+
+    @JavascriptInterface
+    fun location(result: String) {
+        sphere.call("location") {
+            Log.d("my-log", it)
+        }
+    }
+
+    @JavascriptInterface
+    fun overlayClick(result: String) {
+        JsonParser.parseString(result).asJsonObject["data"].asString
+            .let {
+                JsonParser.parseString(
+                    it
+                ).asJsonObject["\$id"]
+            }
+            .asLong
+            .let {
+                SphereMap.SphereObject(" ", id = it)
+            }
+            .run {
+                sphere.call("Overlays.remove", this)
+            }
+    }
+
+    @JavascriptInterface
+    fun error(result: String) {
+        Log.e("my-log", result)
+    }
+
 }
